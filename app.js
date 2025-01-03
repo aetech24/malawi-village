654686f4c846502b2a4d030e77ecf75c323d662e
Stedan1234 <aetech.hub@aetechorg.com>
2025-01-03 14:45:14 +0000

    Comment out dotenv and express-ejs-layouts imports in app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import express from 'express'
 const PORT = 5000 || process.env.PORT
-import expressLayouts from 'express-ejs-layouts';
+// import expressLayouts from 'express-ejs-layouts';
 import router from './server/main.js'
 const app = express();
 
 import { products } from './data/products.js';
 
-//set EJS as the tempalate engine
+//set EJS as the template engine
 app.set('view engine', 'ejs')
 
 //use express ejs layouts
-app.use(expressLayouts)
+// app.use(expressLayouts)
 
 //let the layout file optional 
 app.set('layout', './layouts/main')
